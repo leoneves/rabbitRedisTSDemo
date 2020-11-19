64acff82e773af3b12fe15a221b79550a0b951cd
Leonardo Neves <leonardo.neves@m4u.com.br>
2020-11-19 20:13:49 -0300

    config linter and gitignore

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  useTabs: false,
+};
